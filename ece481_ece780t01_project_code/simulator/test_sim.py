68256a197234555cbd0c1f1c720b49993ceaf793
Anish Khurana <anishkhu264@gmail.com>
2025-06-29 17:34:49 -0400

    Add files via upload

---
+++ ece481_ece780t01_project_code/simulator/test_sim.py
@@ -0,0 +1,9 @@
+import numpy as np
+from flapper_sim import FlapperSim
+
+f = FlapperSim(robot_pose=np.array([1.0, 2.0, 0.8, 1.57]))
+
+while True:
+    y = f.get_output_measurement()
+    
+    f.step(u=np.array([1.0, 2.0, 3.0]))
