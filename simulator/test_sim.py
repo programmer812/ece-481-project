2caa98d0eff66847b1bce54cc5a2d7ea22bfd8dc
Aadila Ali Sabry <aalisabr@uwaterloo.ca>
2025-07-10 18:31:55 -0400

    comment

--- simulator/test_sim.py
+++ simulator/test_sim.py
@@ -3,6 +3,8 @@ from flapper_sim import FlapperSim
 
 f = FlapperSim(robot_pose=np.array([1.0, 2.0, 0.8, 1.57]))
 
+# remember the position, velocity and jerk constraints when computing trajectories
+
 while True:
     y = f.get_output_measurement()
 
