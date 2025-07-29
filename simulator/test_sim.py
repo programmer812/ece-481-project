e033d8a4676d929b3eddf7cdbdcc81e32aa289f0
Aadila Ali Sabry <aalisabr@uwaterloo.ca>
2025-07-29 01:59:06 -0400

    added test for the trajectory

--- simulator/test_sim.py
+++ simulator/test_sim.py
@@ -9,11 +9,9 @@ f = FlapperSim(robot_pose=np.array([1.0, 2.0, 0.8, 1.57]))
 c = Controller()
 
 y = f.get_output_measurement()
-target = np.array([0.5, 0.5, 0.5])
+target = np.array([2, -2, 0.4])
 
-# plot to check correct - want piecewise constant acceleration
 reference = generate_trajectory(y, target)
-print(np.array(reference).shape)
 
 for ref_point in reference:
     time_loop_start = time.time()
---
+++ simulator/test_trajectory.py
@@ -0,0 +1,33 @@
+import numpy as np
+from controller import generate_trajectory
+import matplotlib.pyplot as plt
+from scipy.signal import medfilt
+
+T = 0.1
+
+current = np.array([1.0, 2.0, 0.8])
+target = np.array([2, -2, 0.4])
+
+reference = generate_trajectory(current, target)
+
+for component_idx, component in enumerate(["x", "y", "z"]):
+    ref = [point[component_idx][0] for point in reference]
+    times = [T * i for i in range(len(ref))]
+
+    v = [ref[i + 1] - ref[i] for i in range(len(ref) - 1)]
+    v = medfilt(v, kernel_size=3)
+
+    a = [v[i + 1] - v[i] for i in range(len(v) - 1)]
+    a = medfilt(a, kernel_size=3)
+
+    plt.plot(times, ref)
+    plt.title(f"position - {component}")
+    plt.show(block=True)
+
+    plt.plot(times[:-1], v)
+    plt.title(f"velocity - {component}")
+    plt.show(block=True)
+
+    plt.plot(times[:-2], a)
+    plt.title(f"acceleration - {component}")
+    plt.show(block=True)
