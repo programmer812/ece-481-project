eca229fd497803ad84c2f1741dfa6e01c0c412fc
programmer812 <anishkhu264@gmail.com>
2025-08-02 00:55:15 -0400

    Update robohub folder code

--- robohub/controller.py
+++ robohub/controller.py
@@ -8,19 +8,18 @@ B_d = np.array([[(T**3) / 6], [(T**2) / 2], [T]])
 C_d = np.array([[1, 0, 0]])
 
 L = np.array([[-1.53803172], [-5.92698271], [-10.92812269]])
-F = np.array([[-67.17214498, -51.83430747, -12.47951264]])
+F = np.array([[-87.20516507, -59.06009656, -12.80743744]])
 
-controller_ss_gain = 0.01488712
-trajectory_time = 5
+a = 0.01
 start_time = 1
-end_time = 20
+end_time = 5
 
 
 def generate_trajectory(current, target):
     current = np.reshape(current, (3, 1))
     target = np.reshape(target, (3, 1))
 
-    a = 4 * np.linalg.norm(target - current) / (trajectory_time**2)
+    trajectory_time = math.sqrt((4 * np.linalg.norm(target - current) / a))
     a_with_dir = a * (target - current) / np.linalg.norm(target - current)
 
     k = int(trajectory_time / (2 * T))
@@ -31,8 +30,8 @@ def generate_trajectory(current, target):
     half_position = current + 0.5 * (target - current)
     half_velocity = k * T * a_with_dir
 
-    velocities_part3 = [half_velocity - i * T * a_with_dir for i in range(k)]
-    positions_part3 = [
+    velocities_part2 = [half_velocity - i * T * a_with_dir for i in range(k)]
+    positions_part2 = [
         half_position + i * T * half_velocity - 0.5 * ((i * T) ** 2) * a_with_dir
         for i in range(k)
     ]
@@ -40,23 +39,23 @@ def generate_trajectory(current, target):
     pos_trajectory = (
         [current] * int(start_time / T)
         + positions_part1
-        + positions_part3
+        + positions_part2
         + [target] * int(end_time / T)
     )
 
     v_trajectory = (
         [np.array([[0], [0], [0]])] * int(start_time / T)
         + velocities_part1
-        + velocities_part3
+        + velocities_part2
         + [np.array([[0], [0], [0]])] * int(end_time / T)
     )
 
     accelerations_part1 = [a_with_dir] * len(velocities_part1)
-    accelerations_part3 = [-a_with_dir] * len(velocities_part3)
+    accelerations_part2 = [-a_with_dir] * len(velocities_part2)
     a_trajectory = (
         [np.array([[0], [0], [0]])] * int(start_time / T)
         + accelerations_part1
-        + accelerations_part3
+        + accelerations_part2
         + [np.array([[0], [0], [0]])] * int(end_time / T)
     )
 
@@ -68,7 +67,7 @@ class Controller:
         self.state_estimate = [np.array([[0], [0], [0]]) for _ in range(3)]
         self.u = [0, 0, 0]
 
-    def calculate_acceleration(self, y, position, velocity, acceleration, x):
+    def calculate_acceleration(self, y, position, velocity, acceleration):
         y_hats = [C_d @ x_hat for x_hat in self.state_estimate]
 
         self.state_estimate = [
@@ -78,22 +77,11 @@ class Controller:
             )
         ]
 
-        state = np.reshape(x, (3, 3)).T
         desired_state = np.column_stack((position, velocity, acceleration))
-        # error = desired_state - state
-
-        for x_hat, actual_state in zip(self.state_estimate, state):
-            print(x_hat - np.reshape(actual_state, (3, 1)))
 
         self.u = [
             -F @ (np.reshape(target, (3, 1)) - x_hat)
             for x_hat, target in zip(self.state_estimate, desired_state)
         ]
 
-        # self.u = [
-        #     ref_coord + F @ x_hat for x_hat, ref_coord in zip(self.state_estimate, ref)
-        # ]
-        # u = [-F @ error_component for error_component in error]
-
         return np.array(self.state_estimate).reshape((3, 3)).T.flatten(), self.u
-        # return np.array(self.state_estimate).reshape((3, 3)).T.flatten(), u
--- robohub/test.py
+++ robohub/test.py
@@ -2,6 +2,7 @@ import numpy as np
 from flapper import Flapper
 from controller import Controller, generate_trajectory
 import time
+import json
 
 T = 0.1
 
@@ -13,15 +14,40 @@ target = np.array([0, 0, 0.5])
 
 p_trajectory, v_trajectory, a_trajectory = generate_trajectory(y, target)
 
+actual_p = []
+actual_v = []
+actual_a = []
+
 for position, velocity, acceleration in zip(p_trajectory, v_trajectory, a_trajectory):
     time_loop_start = time.time()
 
     state_estimate, input = c.calculate_acceleration(
-        y, position, velocity, acceleration, f.x
+        y, position, velocity, acceleration
     )
-    f.step(x=state_estimate, u=np.array(input))
+    f.step(x=state_estimate, u=input)
+    
+    actual_p.append(np.copy(f.x[0:3]))
+    actual_v.append(np.copy(f.x[3:6]))
+    actual_a.append(np.copy(f.x[6:9]))
 
     time_loop_end = time.time()
     time.sleep(max(0, T - (time_loop_end - time_loop_start)))
 
     y = f.get_output_measurement()
+    
+p_trajectory = np.reshape(p_trajectory, (len(p_trajectory), 3)).tolist()
+v_trajectory = np.reshape(v_trajectory, (len(v_trajectory), 3)).tolist()
+a_trajectory = np.reshape(a_trajectory, (len(a_trajectory), 3)).tolist()
+
+actual_p = np.reshape(actual_p, (len(actual_p), 3)).tolist()
+actual_v = np.reshape(actual_v, (len(actual_v), 3)).tolist()
+actual_a = np.reshape(actual_a, (len(actual_a), 3)).tolist()
+
+with open("log.json", "w") as f:
+    json.dump(
+        {
+            "trajectory": {"p": p_trajectory, "v": v_trajectory, "a": a_trajectory},
+            "actual": {"p": actual_p, "v": actual_v, "a": actual_a},
+        },
+        f,
+    )
